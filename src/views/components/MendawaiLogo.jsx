24dc9a836c7876b4f90ca9dc71392b1b78b199d4
rizki.malik <rizkimalik94@outlook.com>
2022-06-21 07:48:18 +0700

    update logo

--- src/views/components/MendawaiLogo.jsx
+++ src/views/components/MendawaiLogo.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import logo from 'assets/image/logo-hitam-50.png'
 
-function MendawaiLogo({className}) {
+function MendawaiLogo({ className }) {
     return (
         <>
-            <img src="/assets/media/logos/logo-hitam-50.png" alt="Mendawai-Icon" className={className} />
+            <img src={logo} alt="Keuraisdesk-Icon" className={className} />
+            {/* <img src="/assets/media/logos/logo-hitam-50.png" alt="Mendawai-Icon" className={className} /> */}
         </>
     )
 }
 
 export default MendawaiLogo
+
